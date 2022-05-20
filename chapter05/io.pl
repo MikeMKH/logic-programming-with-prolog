30fcb458754304d746f28335e4f53f7132148a25
Mike Harris <MikeMKH@users.noreply.github.com>
2022-05-20 06:15:36 -0500

    add chapter 5 question 3

--- chapter05/io.pl
+++ chapter05/io.pl
@@ -1,9 +1,9 @@
 makelower :-
   get0(X), process_makelower(X).
 process_makelower(42).
-process_makelower(13) :- nl.
+process_makelower(10) :- nl,makelower.
 process_makelower(X) :-
-  X =\= 13,
+  X =\= 10,
   convert_tolower(X, Y),
   put(Y),makelower.
 convert_tolower(X, Y) :-
@@ -11,3 +11,12 @@ convert_tolower(X, Y) :-
   Y is X + 32.
 convert_tolower(X, X) :- X < 65.
 convert_tolower(X, X) :- X > 90.
+
+readline :-
+  get0(X), process_readline(X).
+process_readline(42).
+process_readline(10) :- write(10),nl,readline.
+process_readline(X) :-
+  X =\= 10,
+  write(X),write(' '),
+  readline.
