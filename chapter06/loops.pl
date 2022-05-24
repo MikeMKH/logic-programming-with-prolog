216702bcf84cb63f9fd24b8b54a5dff777583dd5
Mike Harris <MikeMKH@users.noreply.github.com>
2022-05-24 06:20:31 -0500

    add chapter 6 question 2

--- chapter06/loops.pl
+++ chapter06/loops.pl
@@ -5,3 +5,11 @@ square_range(N1, N2) :-
   write(Sq),nl,
   M is N1 + 1,
   square_range(M, N2).
+
+prompt :-
+  repeat, get0(X), process(X).
+process(10) :- nl.
+process(63) :- nl.
+process(X) :-
+  X \= 10, X \= 63,
+  put(X),fail.
