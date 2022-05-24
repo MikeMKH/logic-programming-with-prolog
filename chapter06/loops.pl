34a6c7037ea41113445f7ae515bc94e36dccb29c
Mike Harris <MikeMKH@users.noreply.github.com>
2022-05-24 06:27:41 -0500

    add chapter 6 question 3
    fix question 2

--- chapter06/loops.pl
+++ chapter06/loops.pl
@@ -9,7 +9,20 @@ square_range(N1, N2) :-
 prompt :-
   repeat, get0(X), process(X).
 process(10) :- nl.
-process(63) :- nl.
+process(63) :-
+  nl,
+  repeat, get0(X), X =:= 10.
 process(X) :-
   X \= 10, X \= 63,
   put(X),fail.
+
+person(john,smith,45,london,doctor).
+person(martin,williams,33,birmingham,teacher).
+person(henry,smith,26,manchester,plumber).
+person(jane,wilson,62,london,teacher).
+person(mary,smith,29,glasgow,surveyor).
+
+over40 :-
+  person(_,_,Age,_,Profession), Age > 40,
+  write(Profession), nl, fail.
+over40.
