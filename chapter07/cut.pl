34b0745c59beccfb8fe92817396075e04ee7222a
Mike Harris <MikeMKH@users.noreply.github.com>
2022-05-25 06:24:44 -0500

    add chapter 7 question 2

--- chapter07/cut.pl
+++ chapter07/cut.pl
@@ -1,3 +1,11 @@
 factorial(1,1) :- !.
 factorial(N,Nfact):-N1 is N-1,
- factorial(N1,Nfact1),Nfact is N*Nfact1. 
+ factorial(N1,Nfact1),Nfact is N*Nfact1.
+
+go :-
+  repeat,read_and_check(N,Type),
+  write(N),write(' is '),write(Type),nl,N=:=100.
+read_and_check(N,Type):-
+  write('Enter next number '),read(N),check(N,Type).
+check(N,even) :- 0 is N mod 2, !.
+check(_,odd).
