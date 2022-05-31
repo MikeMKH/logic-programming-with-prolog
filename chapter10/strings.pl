3438e5ee4bae9aa9c080770cbee1a8cfa8b53d4a
Mike Harris <MikeMKH@users.noreply.github.com>
2022-05-31 06:46:41 -0500

    add chapter 10 question 3

--- chapter10/strings.pl
+++ chapter10/strings.pl
@@ -8,3 +8,8 @@ remove_final(S, R) :-
 remove_ending_spaces([], []) :- !.
 remove_ending_spaces([32|L], R) :- remove_ending_spaces(L, R), !.
 remove_ending_spaces(L, L).
+
+replace_1st_with(C, S, R) :-
+  name(S, L), name(C, X),
+  replace_1st_with_process(X, L, R1), name(R, R1).
+replace_1st_with_process([X], [_|S], [X|S]).
