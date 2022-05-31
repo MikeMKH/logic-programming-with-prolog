192adc2e3052001f43efab8d5911f82704dcb053
Mike Harris <MikeMKH@users.noreply.github.com>
2022-05-31 06:37:57 -0500

    add chapter 10 question 2

--- chapter10/strings.pl
+++ chapter10/strings.pl
@@ -1,3 +1,10 @@
 spalindrome(S) :-
   name(S, L), reverse(L, L).
 
+remove_final(S, R) :-
+  name(S, L), reverse(L, End),
+  remove_ending_spaces(End, X),
+  reverse(X, Y), name(R, Y).
+remove_ending_spaces([], []) :- !.
+remove_ending_spaces([32|L], R) :- remove_ending_spaces(L, R), !.
+remove_ending_spaces(L, L).
